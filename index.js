eb82761874398fd59fbbbae227d71d6e50ee9bf4
Jesse Reynolds <jessereynolds18@gmail.com>
2023-09-30 05:38:06 -0500

    make a POST request using name and email, retrieve the id and append it to the DOM, then use catch to display an error

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(usernameString, emailString){
+    let destinationUrl = "http://localhost:3000/users"
+
+    const formData = {
+        name: usernameString,
+        email: emailString,
+    }
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    }
+    return fetch(destinationUrl, configurationObject)
+    .then((response) => response.json())
+    .then((data) => {
+        const showIdNumber = data.id;
+        const createElement = document.createElement('p');
+        const body = document.body;
+        body.append(createElement);
+        createElement.textContent = `${showIdNumber}`;
+    })
+    .catch((error) => {
+        let message = 'Unauthorized Access';
+        const createElement = document.createElement('p');
+        const body = document.body;
+        body.append(createElement);
+        createElement.textContent = message;
+    });
+}
